89c38fde09bc8525944fbb43268be72dbaac3d75
alexmonari <124360715+alexmonari@users.noreply.github.com>
2023-02-04 12:08:23 +0000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -4,6 +4,9 @@ input.onButtonPressed(Button.A, function () {
 input.onButtonPressed(Button.AB, function () {
     music.playMelody("C5 B A G F E D C ", 120)
 })
+input.onButtonPressed(Button.B, function () {
+    music.playMelody("C5 A B G A F G E ", 120)
+})
 input.onGesture(Gesture.Shake, function () {
     music.playMelody("C5 G B A F A C5 B ", 120)
 })
