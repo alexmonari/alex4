8eb0bcfa7ac743d460ff634979a016c4bf1ea2a0
alexmonari <124360715+alexmonari@users.noreply.github.com>
2023-02-04 12:04:28 +0000

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,10 @@
+input.onButtonPressed(Button.A, function () {
+    music.playMelody("E B C5 A B G A F ", 120)
+})
+input.onButtonPressed(Button.AB, function () {
+    music.playMelody("C5 B A G F E D C ", 120)
+})
+input.onGesture(Gesture.Shake, function () {
+    music.playMelody("C5 G B A F A C5 B ", 120)
+})
+basic.showIcon(IconNames.Ghost)
